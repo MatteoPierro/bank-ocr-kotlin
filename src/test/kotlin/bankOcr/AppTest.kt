42de0382ea8df1c430b563e1cf9cc07e288c9c93
Matteo Pierro <pierromatteo@gmail.com>
2020-03-03 10:12:35 +0100

    initial commit

---
+++ src/test/kotlin/bankOcr/AppTest.kt
@@ -0,0 +1,12 @@
+package bankOcr
+
+import assertk.assertThat
+import assertk.assertions.isFalse
+import org.junit.jupiter.api.Test
+
+class AppTest {
+    @Test
+    internal fun `fix me`() {
+        assertThat(true).isFalse()
+    }
+}
