6b8191c6cfe14cf6c32e41fbc7ce4ddc0b471dd2
Matteo Pierro <pierromatteo@gmail.com>
2020-03-04 19:48:58 +0100

    more extension functions

--- src/main/kotlin/bankOcr/EntriesReader.kt
+++ src/main/kotlin/bankOcr/EntriesReader.kt
@@ -5,33 +5,34 @@ class EntriesReader(private val linesReader: LinesReader) {
     fun readAll(): Entries {
         val lines = linesReader.readLines()
 
-        val entries = entriesFrom(lines)
-        return Entries(entries)
-    }
-
-    private fun entriesFrom(lines: Lines): List<Entry> {
-        val rawEntries = lines.value.chunked(ENTRY_SIZE)
-        return rawEntries.map { rawEntry ->
-            rawEntry.map { line -> line.chunked(BLOCK_SIZE) }
-        }.map { toEntry(it) }
-    }
-
-    private fun toEntry(rawEntry: RawEntry): Entry {
-        val blocks = rawEntry.blocksIndexes()
-                .map { blockIndex ->
-                    rawEntry.joinToString(separator = "\n") { it[blockIndex] }
-                }
-                .map { Block(it) }
-        return Entry(blocks)
+        return lines.toEntries(ENTRY_SIZE, BLOCK_SIZE)
     }
 
     companion object {
-
         private const val ENTRY_SIZE = 4
         private const val BLOCK_SIZE = 3
     }
 }
 
 private typealias RawEntry = List<List<String>>
+
+private fun Lines.toEntries(entrySize: Int, blockSize: Int) =
+        toRawEntries(entrySize, blockSize)
+                .map { it.toEntry() }
+                .let { Entries(it) }
+
+private fun Lines.toRawEntries(entrySize: Int, blockSize: Int) =
+        value.chunked(entrySize)
+                .map { rawEntry ->
+                    rawEntry.map { line -> line.chunked(blockSize) }
+                }
+
+private fun RawEntry.toEntry() =
+        blocksIndexes()
+                .map { blockIndex ->
+                    this.joinToString(separator = "\n") { it[blockIndex] }
+                }
+                .map { Block(it) }
+                .let { Entry(it) }
 
 private fun RawEntry.blocksIndexes() = this.component1().indices
