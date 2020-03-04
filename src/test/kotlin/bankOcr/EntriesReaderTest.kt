ed729b942d42c041fe111e2881e81a8756f1fe92
Matteo Pierro <pierromatteo@gmail.com>
2020-03-04 18:44:27 +0100

    use linesReader mock in test

--- src/test/kotlin/bankOcr/EntriesReaderTest.kt
+++ src/test/kotlin/bankOcr/EntriesReaderTest.kt
@@ -2,12 +2,20 @@ package bankOcr
 
 import assertk.assertThat
 import assertk.assertions.containsExactly
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import java.io.File
 
+@ExtendWith(MockKExtension::class)
 class EntriesReaderTest {
     private val inputFile = "input.txt"
+
+    @RelaxedMockK
+    lateinit var linesReader: LinesReader
 
     @BeforeEach
     internal fun setUp() {
@@ -16,18 +24,20 @@ class EntriesReaderTest {
 
     @Test
     internal fun `it reads an entry with one block`() {
-        File(inputFile).bufferedWriter().use { out ->
-            out.write("   \n")
-            out.write("  |\n")
-            out.write("  |\n")
-            out.write("   \n")
-        }
+        every { linesReader.readLines() }
+                .returns(
+                        Lines(listOf(
+                                "   ",
+                                "  |",
+                                "  |",
+                                "   "
+                        )))
 
-        val entries = EntriesReader(LinesReader(inputFile)).readAll()
+        val entries = EntriesReader(linesReader).readAll()
 
         assertThat(entries.value)
                 .containsExactly(
-                    Entry(listOf(Block.one))
+                        Entry(listOf(Block.one))
                 )
     }
 
@@ -44,7 +54,7 @@ class EntriesReaderTest {
 
         assertThat(entries.value)
                 .containsExactly(
-                    Entry(listOf(Block.one, Block.two))
+                        Entry(listOf(Block.one, Block.two))
                 )
     }
 }
