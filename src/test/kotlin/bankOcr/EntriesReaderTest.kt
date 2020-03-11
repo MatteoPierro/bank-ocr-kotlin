7456d4741d966d56e4074e5ececc43f3afddd630
Matteo Pierro <pierromatteo@gmail.com>
2020-03-11 19:33:44 +0100

    test collaboration between EntriesReader and LinesReader

--- src/test/kotlin/bankOcr/EntriesReaderTest.kt
+++ src/test/kotlin/bankOcr/EntriesReaderTest.kt
@@ -5,6 +5,7 @@ import assertk.assertions.containsExactly
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -14,11 +15,23 @@ class EntriesReaderTest {
     @RelaxedMockK
     lateinit var linesReader: LinesReader
 
+    @RelaxedMockK
+    lateinit var linesParser: LinesParser
+
     private lateinit var entriesReader: EntriesReader
 
     @BeforeEach
     internal fun setUp() {
         entriesReader = EntriesReader(linesReader)
+    }
+
+    @Test
+    internal fun `it reads lines`() {
+        val entriesReader = EntriesReader(linesReader, linesParser)
+
+        entriesReader.readAll()
+
+        verify { linesReader.readLines() }
     }
 
     @Test
